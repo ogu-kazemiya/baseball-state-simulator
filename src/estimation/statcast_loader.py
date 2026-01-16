639548e8e13b57ee8fc6a01de7741357c9236eef
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2026-01-16 19:54:03 +0900

    statcast_loaderを改善

--- src/estimation/statcast_loader.py
+++ src/estimation/statcast_loader.py
@@ -1,5 +1,7 @@
 import os
+import time
 from pathlib import Path
+from datetime import datetime, timedelta
 import pandas as pd
 from tqdm import tqdm
 from pybaseball import statcast, cache
@@ -9,7 +11,6 @@ import src.estimation.utils as utils
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 STATCAST_DATA_DIR = PROJECT_ROOT / "data" / "statcast"
 
-# 指定した期間のstatcastデータを取得
 def get_statcast(
     start_year: int,
     end_year: int | None = None,
@@ -18,39 +19,48 @@ def get_statcast(
     if end_year is None:
         end_year = start_year
     columns = list(dict.fromkeys(columns)) if columns is not None else consts.REQUIRED_COLS
+    years = range(start_year, end_year + 1)
+
+    for year in years:
+        ensure_season_statcast(year)
 
     df_list = []
-    years = range(start_year, end_year + 1)
-    for year in tqdm(years, desc="Loading Seasons"):
-        df_year = get_season_statcast(year, columns=columns)
+    for year in years:
+        df_year = get_season_statcast(year, columns)
         df_list.append(df_year)
 
     return pd.concat(df_list, ignore_index=True)
 
-# 指定した年のstatcastデータを取得
-def get_season_statcast(year: int, columns: list[str] | None = None) -> pd.DataFrame:
-    columns = list(dict.fromkeys(columns)) if columns is not None else consts.REQUIRED_COLS
+def get_season_statcast(year: int, columns: list[str]) -> pd.DataFrame:
+    file_path = STATCAST_DATA_DIR / f"statcast_{year}.parquet"
 
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Statcast data for year {year} not found in cache.")
+
+    df = pd.read_parquet(file_path, columns=columns)
+    utils.validate_columns(df, columns or [], context=f"get_season_statcast_({year}) cache")
+    return df
+
+def ensure_season_statcast(year: int) -> None:
     os.makedirs(STATCAST_DATA_DIR, exist_ok=True)
     file_path = STATCAST_DATA_DIR / f"statcast_{year}.parquet"
 
-    # キャッシュが存在すれば読み込み
     if os.path.exists(file_path):
-        try:
-           return pd.read_parquet(file_path, columns=columns)
-        except Exception as e:
-           df = pd.read_parquet(file_path)
-           utils.validate_columns(df, columns or [], context=f"get_season_statcast({year}) cache")
-           raise e
+        return
 
-    # データを取得して保存
     cache.enable()
-    start_date = f"{year}-03-01"
-    end_date = f"{year}-11-30"
-    df = statcast(start_dt=start_date, end_dt=end_date)
-    df.to_parquet(file_path)
+    months = range(3, 12)
+    df_list = []
 
-    utils.validate_columns(df, columns or [], context=f"get_season_statcast({year}) fetch")
-    if columns is not None:
-        df = df[columns]
-    return df
+    for month in tqdm(months, desc=f"Downloading Statcast data for {year}"):
+        start_date = datetime(year, month, 1)
+        end_date = datetime(year, month + 1, 1) - timedelta(days=1)
+        str_start_date = start_date.strftime("%Y-%m-%d")
+        str_end_date = end_date.strftime("%Y-%m-%d")
+
+        df_month = statcast(start_dt=str_start_date, end_dt=str_end_date, verbose=False)
+        df_list.append(df_month)
+        time.sleep(1)
+
+    df = pd.concat(df_list, ignore_index=True)
+    df.to_parquet(file_path)
